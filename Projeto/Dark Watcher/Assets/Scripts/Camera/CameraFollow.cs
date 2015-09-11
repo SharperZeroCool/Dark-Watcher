76791854ebd397cf83be59388994a67ae411a86a
sharperzerocool <sharperzerocool@gmail.com>
2015-09-11 17:40:50 -0300

    Criado projeto Unity Criado script de controle de camera Criado script de movimento 2D do jogador Criado cena simples Adicionado "Standard Assets" do Unity para efeitos graficos Adicionado "Standard Assets" do Unity para sistema de particulas Criado material e texturas simples para o chão

---
+++ Projeto/Dark Watcher/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public bool useDefaultCameraOffset;
+
+	public bool useMouseRotation;
+
+	public bool useMouseZoom;
+
+	public float mouseSmoothing = 5f;
+
+	public float cameraMinimumZoomDistance = 50f;
+
+	public float cameraMaximumZoomDistance = 200f;
+
+	private Vector3 offset;
+
+	private void Start() {
+		Application.targetFrameRate = -1;
+		if ( target == null ) {
+			target = GameObject.FindGameObjectWithTag("Player").transform;
+		}
+
+		if ( useDefaultCameraOffset ) {
+			transform.position = target.position + new Vector3(7, 3, 0);
+			transform.LookAt(target.position + Vector3.up);
+		}
+
+		offset = target.position - transform.position;
+
+	}
+
+	private void Update() {
+		UpdateCameraZoom();
+
+		UpdateCameraPosition();
+
+		UpdateCameraRotation();
+
+		UpdateCameraOffset();
+	}
+
+	private void UpdateCameraPosition() {
+		transform.position = target.position - offset;
+	}
+
+	private void UpdateCameraRotation() {
+		if ( useMouseRotation )
+			transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * mouseSmoothing);
+	}
+
+	private void UpdateCameraZoom() {
+		if ( !useMouseZoom )
+			return;
+
+		float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+		if ( ZoomIn(scrollWheel) ) {
+			if ( offset.sqrMagnitude > cameraMinimumZoomDistance )
+				offset *= 0.9f;
+
+		} else if ( ZoomOut(scrollWheel) ) {
+			if ( offset.sqrMagnitude < cameraMaximumZoomDistance )
+				offset *= 1.1f;
+
+		}
+	}
+
+	private void UpdateCameraOffset() {
+		offset = target.position - transform.position;
+	}
+
+	private bool ZoomIn(float scrollWheel) {
+		return scrollWheel > 0f;
+	}
+
+	private bool ZoomOut(float scrollWheel) {
+		return scrollWheel < 0f;
+	}
+
+}
+
---
+++ Projeto/Dark Watcher/Assets/Scripts/Player/Movement/CharacterController/CCPlayerMovement2D.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCPlayerMovement2D : CCPlayerMovement {
+
+	private void Update() {
+		bool jumpPressed = Input.GetButtonDown("Jump");
+
+		if ( jumpPressed && IsAllowedToJump() ) {
+			jump = true;
+			hasDoubleJumped = false;
+		} else if ( jumpPressed && IsAllowedToDoubleJump() ) {
+			doubleJump = true;
+			hasDoubleJumped = true;
+		}
+
+	}
+
+	private void FixedUpdate() {
+
+		ResetFrameVariables();
+
+		FreezeXPosition();
+
+		if ( !HasHorizontalMovement() && characterController.isGrounded ) {
+			SlowDownGround();
+
+		}
+
+		horizontalAxis = Input.GetAxisRaw("Horizontal");
+		verticalAxis = Input.GetAxisRaw("Vertical");
+		jumpButton = Input.GetButton("Jump");
+		runningButton = Input.GetButton("Sprint");
+
+		Move();
+		Flip();
+		Jump();
+
+		if ( ShouldApplyGravity() ) {
+			Gravity();
+		}
+
+		if ( characterController.isGrounded ) {
+			hasDoubleJumped = false;
+		}
+
+		LimitHorizontalSpeed();
+		LimitVerticalSpeed();
+
+		characterController.Move(movement * Time.fixedDeltaTime);
+
+	}
+
+	private void Move() {
+		if ( isInWater || isInWaterSurface ) {
+			Swim();
+			return;
+		}
+
+		if ( characterController.isGrounded ) {
+			GroundMovement();
+		} else {
+			AirMovement();
+		}
+
+	}
+
+	private void Flip() {
+		if ( HasHorizontalMovement() ) {
+			float ratio = horizontalAxis + transform.forward.z;
+
+			if ( ratio >= -1 && ratio <= 1 ) {
+				Quaternion rotation = Quaternion.LookRotation(transform.forward * -1);
+				transform.rotation = rotation;
+
+				movement.z = 0;
+			}
+		}
+	}
+
+	private void Gravity() {
+		movement.y -= gravity * Time.fixedDeltaTime;
+	}
+
+	private void LimitHorizontalSpeed() {
+		float maxSpeed;
+
+		if ( runningButton ) {
+			maxSpeed = base.maxRunningSpeed;
+		} else {
+			maxSpeed = base.maxSpeed;
+		}
+
+		if ( isInWater || isInWaterSurface ) {
+			maxSpeed = base.maxSwimSpeed;
+		} else if ( !characterController.isGrounded ) {
+			maxSpeed = base.maxMidAirSpeed;
+		}
+
+
+		if ( Mathf.Abs(movement.z) > maxSpeed ) {
+			movement.z = maxSpeed * transform.forward.z;
+		}
+	}
+
+	private void LimitVerticalSpeed() {
+		if ( IsGrabbingWall() && movement.y < maxSlideDownSpeed ) {
+			movement.y += slideDownSpeed * Time.fixedDeltaTime;
+			if ( movement.y >= maxSlideDownSpeed ) {
+				movement.y = maxSlideDownSpeed;
+			}
+		}
+	}
+
+	private void Swim() {
+		if ( HasHorizontalMovement() ) {
+			HorizontalMovement(swimHorizontalSpeed);
+		} else {
+			SlowDownHorizontalSpeedSwim();
+		}
+
+		if ( HasVerticalMovement() ) {
+			if ( !isInWater && verticalAxis > 0 ) {
+				return;
+			}
+			VerticalMovement(swimVerticalSpeed);
+		} else {
+			SlowDownVerticalSpeed();
+		}
+
+	}
+
+	private void GroundMovement() {
+		float speed;
+
+		movement.y = 0;
+
+		if ( runningButton ) {
+			speed = base.runningSpeed;
+		} else {
+			speed = base.speed;
+		}
+
+		HorizontalMovement(speed);
+
+	}
+
+	private void AirMovement() {
+		if ( !HasHorizontalMovement() ) {
+			SlowDownHorizontalSpeedMidAir();
+			return;
+
+		}
+
+		movement.z += horizontalAxis * Time.fixedDeltaTime * midAirMovSpeed;
+	}
+
+	private void SlowDownHorizontalSpeedSwim() {
+		SlowDownHorizontalSpeed(waterMovementLost);
+	}
+
+	private void SlowDownHorizontalSpeedMidAir() {
+		if ( Mathf.Abs(movement.z) > 1 ) {
+			SlowDownHorizontalSpeed(mirAirMovementLost);
+		}
+	}
+
+	private void SlowDownHorizontalSpeed(float amount) {
+		movement.z -= movement.z / Mathf.Abs(movement.z) * Time.fixedDeltaTime * amount;
+	}
+
+	private void SlowDownVerticalSpeed() {
+		if ( Mathf.Abs(movement.y) > 1 ) {
+			movement.y -= movement.y / Mathf.Abs(movement.y) * Time.fixedDeltaTime * midAirMovSpeed;
+		}
+	}
+
+	private void SlowDownGround() {
+		movement.z = 2 * transform.forward.z * Time.fixedDeltaTime;
+	}
+
+	private void HorizontalMovement(float speed) {
+		movement.z += horizontalAxis * speed;
+	}
+
+	private void VerticalMovement(float speed) {
+		movement.y += verticalAxis * speed;
+	}
+
+	private void FreezeXPosition() {
+		Vector3 position = transform.position;
+		position.x = 0;
+		transform.position = position;
+	}
+}
