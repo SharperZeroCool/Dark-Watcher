ff6c4ca9f98a449a010b5bf4a2eedf18176b131a
sharperzerocool <sharperzerocool@gmail.com>
2015-09-26 03:39:29 +0800

    Menu de Pause Mixer do level de audio

--- Projeto/Dark Watcher/Assets/Scripts/Managers/PauseManager.cs
+++ Projeto/Dark Watcher/Assets/Scripts/Managers/PauseManager.cs
@@ -7,11 +7,11 @@ using UnityEditor;
 #endif
 
 public class PauseManager : MonoBehaviour {
-	
+
 	public AudioMixerSnapshot paused;
 
 	public AudioMixerSnapshot unpaused;
-	
+
 	private Canvas canvas;
 
 	private RectTransform rectTransform;
@@ -22,96 +22,48 @@ public class PauseManager : MonoBehaviour {
 
 	private const int MIN_HEIGHT = 310;
 
-	public void Quit()
-	{
-		#if UNITY_EDITOR 
+	private void Start() {
+		canvas = GetComponent<Canvas>();
+		definePauseMenuResolution();
+		Time.timeScale = 1;
+	}
+
+	private void Update() {
+		if ( Input.GetKeyDown(KeyCode.Escape) ) {
+			canvas.enabled = !canvas.enabled;
+			Pause();
+		}
+	}
+
+	public void Quit() {
+		#if UNITY_EDITOR
 		EditorApplication.isPlaying = false;
-		#else 
+		#else
 		Application.Quit();
 		#endif
 	}
 
-	public void Pause()
-	{
+	public void Pause() {
 		Time.timeScale = Time.timeScale == 0 ? 1 : 0;
-		Lowpass ();
-		
-	}
-	
-	private void Start()
-	{
-
-		canvas = GetComponent<Canvas>();
-
-		definePauseMenuResolution ();
-
-		Time.timeScale = 1;
+		Lowpass();
 
 	}
 
-	private void definePauseMenuResolution()
-	{
-
-		rectTransform = GameObject.FindGameObjectWithTag ("PausePanel").GetComponent<RectTransform>();
-		
+	private void definePauseMenuResolution() {
+		rectTransform = GameObject.FindGameObjectWithTag("PausePanel").GetComponent<RectTransform>();
 		float width = Screen.width * DEFAULT_SCREEN_TO_MENU_RATIO;
-		
 		float height = Screen.height * DEFAULT_SCREEN_TO_MENU_RATIO;
-		
 		width = width < MIN_WIDTH ? MIN_WIDTH : width;
-		
 		height = height < MIN_HEIGHT ? MIN_HEIGHT : height;
-		
 		rectTransform.sizeDelta = new Vector2(width, height);
-
 	}
-	
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
 
-			canvas.enabled = !canvas.enabled;
-
-			Cursor.visible = !Cursor.visible;
-
-			if(isPaused())
-			{
-
-				Cursor.lockState = CursorLockMode.None;
-
-			}
-			else 
-			{
-
-				Cursor.lockState = CursorLockMode.Locked;
-
-			}
-
-			Pause();
-
-		}
-	}
-		
-	private void Lowpass()
-	{
-		if (Time.timeScale == 0)
-		{
+	private void Lowpass() {
+		if ( Time.timeScale == 0 ) {
 			paused.TransitionTo(.01f);
-		}
-		
-		else
-			
-		{
+		} else {
 			unpaused.TransitionTo(.01f);
 		}
-	}
-
-	private bool isPaused()
-	{
-
-		return Cursor.visible;
-
 	}
 
 }
