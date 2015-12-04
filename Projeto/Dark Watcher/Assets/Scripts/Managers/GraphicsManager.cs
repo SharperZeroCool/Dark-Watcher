0d2e9b51c2d66e7d504b640975e6b03564e791ca
sharperzerocool <sharperzerocool@gmail.com>
2015-12-04 18:16:12 -0200

    Ajustes na Quarta Release

---
+++ Projeto/Dark Watcher/Assets/Scripts/Managers/GraphicsManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicsManager : MonoBehaviour {
+
+	public GameObject[] effectsToDisable;
+
+	void Start () {
+		int qualityLevel = QualitySettings.GetQualityLevel();
+		if(qualityLevel <= 4) {
+			for(int i = 0; i < effectsToDisable.Length; i++ ) {
+				effectsToDisable[i].SetActive(false);
+			}
+		}
+	}
+	
+}
